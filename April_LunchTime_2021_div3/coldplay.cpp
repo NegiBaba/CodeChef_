68be42f4e524dc1cff40332eafd3006572f144f2
NegiBaba <piyushnegi319@gmail.com>
2021-05-01 20:35:40 +0530

    Added folder for lunchtime April 2021

---
+++ April_LunchTime_2021_div3/coldplay.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+#define lli long long int
+#define endl '\n'
+
+#define PI 3.141
+#define MOD 1000000009
+
+using namespace std;
+
+int main()
+{
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+	
+	fastio;
+	int testcases; cin >> testcases;
+	while(testcases--)
+	{
+		int m, s; 
+		cin >> m >> s;
+
+		cout << (m / s) << endl;
+	}
+	return 0;
+}
